b21840beaf406b0aee40985d8fc7432ed784ae52
JAKOwen-gh <64730020+JAKOwen-gh@users.noreply.github.com>
2023-05-17 15:53:57 +0100

    Made shape 3D, added rotation and eased animation to be smoother

--- sketch.js
+++ sketch.js
@@ -1,28 +1,27 @@
 let song, analyzer;
 let audio, fft;
+let sphereSize = 100;
 
 function preload() {
   song = loadSound('N217.mp3');
 }
 
 function setup() {
-  createCanvas(1600, 1600);
+  createCanvas(1600, 1600, WEBGL); // Set up a WebGL canvas for 3D rendering
   song.loop();
 
   // Amplitude analyzer
   analyzer = new p5.Amplitude();
 
-  // Connects input to an volume analyzer
+  // Connects input to a volume analyzer
   analyzer.setInput(song);
 
-  //for audio
+  // Audio input and FFT
   audio = new p5.AudioIn();
   audio.start();
   fft = new p5.FFT();
   fft.setInput(audio);
 }
-
-
 
 function draw() {
   background(255);
@@ -31,25 +30,26 @@ function draw() {
   let volumeInput = analyzer.getLevel();
   stroke(0);
 
-  // Draw shape with size based on volume
-  circle(width / 2, height / 2, 100 + volumeInput * windowWidth, 100 + volumeInput * windowHeight);
+  // Apply easing to smooth out size changes
+  let targetSize = 100 + volumeInput * min(windowWidth, windowHeight);
+  let easing = 0.05;
+  sphereSize += (targetSize - sphereSize) * easing;
 
+  rotateY(frameCount * 0.01); // Rotate the sphere
+  sphere(sphereSize);
 
   let spectrum = fft.analyze();
 
   beginShape();
-  for (i = 0; i < spectrum.length; i++) {
-    vertex(i, map(spectrum[i], 0, 455, height, 0));
+  for (let i = 0; i < spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0));
   }
   endShape();
 }
 
-
-
 // Play Button
 function mousePressed() {
   if (song.isPlaying()) {
-    // .isPlaying() returns a boolean
     song.stop();
     background(255, 0, 0);
   } else {
