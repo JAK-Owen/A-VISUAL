981ffcfda41a72b902402ee23f6f58d7d0a07a96
JAKOwen-gh <64730020+JAKOwen-gh@users.noreply.github.com>
2023-05-25 16:14:41 +0100

    Commented code

--- sketch.js
+++ sketch.js
@@ -1,134 +1,128 @@
-let song, analyzer;
-let shapeSize = { width: 1600, height: 1600, depth: 1600 };
-let targetshapeSize = { width: 1600, height: 1600, depth: 1600 };
-let easing = 0.0001;
-let filteredAmplitude = 0.0;
-let filterCoefficient = 0.02;
+// Declare variables and objects
+let song, analyzer; // Audio-related objects
+let shapeSize = { width: 1600, height: 1600, depth: 1600 }; // Current shape size
+let targetShapeSize = { width: 1600, height: 1600, depth: 1600 }; // Target shape size
+let easing = 0.01; // Easing value for smooth transitions
+let filteredAmplitude = 0.0; // Filtered audio amplitude
+let filterCoefficient = 0.02; // Coefficient for the low-pass filter
 
-let currentShapeIndex = 0;
-let targetShapeIndex = 0;
-let shapes = ["box", "sphere", "cylinder", "cone", "ellipsoid", "torus"];
+let currentShapeIndex = 0; // Current shape index
+let targetShapeIndex = 0; // Target shape index
+let shapes = ["box", "sphere", "cylinder", "cone", "ellipsoid", "torus"]; // Available shapes
 
-let shapeTimer = 0;
-let shapeDuration = 300;
+let shapeTimer = 0; // Timer for shape transitions
+let shapeDuration = 300; // Duration for each shape
 
+let interpolationFactor = 0.0; // Factor for shape interpolation
+
+// Preload function to load the audio file
 function preload() {
   song = loadSound('N217.mp3');
 }
 
 function setup() {
-  smooth();
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  song.loop();
-  analyzer = new p5.Amplitude();
-  analyzer.setInput(song);
+  createCanvas(windowWidth, windowHeight, WEBGL); // Create a 3D canvas
+  song.loop(); // Start playing the audio in a loop
+  analyzer = new p5.Amplitude(); // Create an amplitude analyzer object
+  analyzer.setInput(song); // Connect the audio input to the analyzer
 }
 
 function draw() {
-  background(0);
+  background(0); // Set the background color to black (RGB 0)
 
-  let amplitude = analyzer.getLevel();
-  filteredAmplitude += (amplitude - filteredAmplitude) * filterCoefficient;
+  let amplitude = analyzer.getLevel(); // Get the current audio amplitude
+  filteredAmplitude += (amplitude - filteredAmplitude) * filterCoefficient; // Smooth the amplitude using a low-pass filter
 
+  // Map the filtered amplitude to a target shape size
   let targetSize = map(filteredAmplitude, 0, 1, 100, 800);
   targetSize = max(targetSize, 1600);
 
+  // Randomly change the target shape and reset the shape timer
   if (random() < 0.001) {
-    targetshapeSize.width = random(max(targetSize * 0.0, 1600), targetSize * 0.5);
-    targetshapeSize.height = random(max(targetSize * 0.0, 1600), targetSize * 0.5);
-    targetshapeSize.depth = random(max(targetSize * 0.0, 1600), targetSize * 0.5);
+    targetShapeSize.width = random(max(targetSize * 0.0, 1600), targetSize * 0.5);
+    targetShapeSize.height = random(max(targetSize * 0.0, 1600), targetSize * 0.5);
+    targetShapeSize.depth = random(max(targetSize * 0.0, 1600), targetSize * 0.5);
 
     targetShapeIndex = floor(random(shapes.length));
 
     shapeTimer = 0; // Reset the shape timer
   }
 
-  shapeSize.width += (targetshapeSize.width - shapeSize.width) * easing;
-  shapeSize.height += (targetshapeSize.height - shapeSize.height) * easing;
-  shapeSize.depth += (targetshapeSize.depth - shapeSize.depth) * easing;
+  // Smoothly adjust the shape size towards the target size using easing
+  shapeSize.width += (targetShapeSize.width - shapeSize.width) * easing;
+  shapeSize.height += (targetShapeSize.height - shapeSize.height) * easing;
+  shapeSize.depth += (targetShapeSize.depth - shapeSize.depth) * easing;
 
-  let morphedShape = morphShape(currentShapeIndex, targetShapeIndex, easing);
+  // Interpolate the shape index and size based on the current and target values
+  let morphedShapeIndex = lerp(currentShapeIndex, targetShapeIndex, interpolationFactor);
+  let morphedShapeSize = {
+    width: lerp(shapeSize.width, targetShapeSize.width, interpolationFactor),
+    height: lerp(shapeSize.height, targetShapeSize.height, interpolationFactor),
+    depth: lerp(shapeSize.depth, targetShapeSize.depth, interpolationFactor)
+  };
 
-  rotateY(frameCount * 0.001);
-  rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001); // Rotate the 3D scene around the Y-axis
+  rotateX(frameCount * 0.001); // Rotate the 3D scene around the X-axis
 
-  let scaleValue = map(filteredAmplitude, 0, 1, 0.1, 2);
-  scale(scaleValue);
+  let scaleValue = map(filteredAmplitude, 0, 1, 0.1, 2); // Map the filtered amplitude to a scale value
+  scale(scaleValue); // Apply the scale to the 3D scene
 
-  stroke(255);
-  strokeWeight(1);
-  noFill();
-  ambientMaterial(255);
-  specularMaterial(255);
+  stroke(255); // Set the stroke color to white
+  strokeWeight(1); // Set the stroke weight to 1
+  noFill(); // Disable filling shapes
+  ambientMaterial(255); // Set the ambient material to white
+  specularMaterial(255); // Set the specular material to white
 
-  // Draw the current shape
-  drawShape(currentShapeIndex);
+  drawShape(morphedShapeIndex, morphedShapeSize); // Draw the current shape
 
-  // Increment the shape timer
-  shapeTimer++;
+  shapeTimer++; // Increment the shape timer
 
-  // If the shape timer exceeds the duration, update the current shape
+  // If the shape timer exceeds the duration, update the current shape and reset the interpolation factor
   if (shapeTimer >= shapeDuration) {
     currentShapeIndex = targetShapeIndex;
+    interpolationFactor = 0.0; // Reset the interpolation factor
   }
 
-  // Interpolate the shape towards the target shape
-  if (currentShapeIndex !== targetShapeIndex) {
-    let interpolation = shapeTimer / shapeDuration;
-    let intermediateShape = morphShape(currentShapeIndex, targetShapeIndex, interpolation);
-    if (intermediateShape !== currentShapeIndex) {
-      push();
-      let remainingTime = shapeDuration - shapeTimer;
-      let remainingProgress = remainingTime / shapeDuration;
-      let scaleFactor = map(remainingProgress, 0, 1, 0, 1);
-      scale(scaleFactor);
-      drawShape(intermediateShape);
-      pop();
-    }
+  // Gradually increase the interpolation factor during shape transitions
+  if (currentShapeIndex !== targetShapeIndex && interpolationFactor < 0.0) {
+    interpolationFactor += 0.001;
   }
 }
 
-function morphShape(startIndex, endIndex, amount) {
-  let morphedIndex = startIndex;
-
-  if (startIndex !== endIndex) {
-    morphedIndex = lerp(startIndex, endIndex, amount);
-
-    if (morphedIndex >= shapes.length) {
-      morphedIndex -= shapes.length;
-    }
-  }
-
-  return morphedIndex;
-}
-
-function drawShape(index) {
+// Function to draw a shape based on its index and size
+function drawShape(index, size) {
   switch (shapes[index]) {
     case "box":
-      box(shapeSize.width, shapeSize.height, shapeSize.depth);
+      box(size.width, size.height, size.depth);
       break;
     case "sphere":
-      sphere(max(shapeSize.width, shapeSize.height, shapeSize.depth) / 2);
+      sphere(max(size.width, size.height, size.depth) / 2);
       break;
     case "cylinder":
-      cylinder(shapeSize.width / 2, shapeSize.height);
+      cylinder(size.width / 2, size.height);
       break;
     case "cone":
-      cone(shapeSize.width / 2, shapeSize.height);
+      cone(size.width / 2, size.height);
       break;
     case "ellipsoid":
-      ellipsoid(shapeSize.width / 2, shapeSize.height / 2, shapeSize.depth / 2);
+      ellipsoid(size.width / 2, size.height / 2, size.depth / 2);
       break;
     case "torus":
-      torus(shapeSize.width / 2, shapeSize.height / 2);
+      torus(size.width / 2, size.height / 2);
       break;
   }
 }
 
+// Toggle audio playback when the mouse is pressed
 function mousePressed() {
   if (song.isPlaying()) {
     song.stop();
   } else {
     song.play();
   }
+}
+
+// Resize the canvas when the window size changes
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
